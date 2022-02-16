a7a8dab88aa5da0f33c7ea61ac0b37c90622d13d
Vietcuong0508 <73566409+Vietcuong0508@users.noreply.github.com>
2022-02-16 23:01:44 +0700

    update

---
+++ ObserverDemo/IObserver.cs
@@ -0,0 +1,8 @@
+﻿namespace ObserverDemo
+{
+    public interface IObserver
+    {
+        // Nhận thông tin cập nhật từ chủ thể
+        void Update(ISubject subject);
+    }
+}
---
+++ ObserverDemo/ISubject.cs
@@ -0,0 +1,14 @@
+﻿namespace ObserverDemo
+{
+    public interface ISubject
+    {
+        // Gắn một observer đến chủ thể.
+        void Attach(IObserver observer);
+
+        // Tách một observer từ chủ thể.
+        void Detach(IObserver observer);
+
+        // Thông báo tất cả observers về sự kiện.
+        void Notify();
+    }
+}
